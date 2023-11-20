f525c5f1f6f249037866e9e12b79e6f37a60e4d7
Olaf Yang <mail@olafyang.com>
2023-11-20 22:38:01 +0100

    initial commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+	return (
+		<>
+			<nav>
+				<h1>
+					<a>Name</a>
+				</h1>
+				<p>
+					<a>Item 1</a>
+				</p>
+				<p>
+					<a>Item 2</a>
+				</p>
+				<p>
+					<a>Item 3</a>
+				</p>
+			</nav>
+		</>
+	);
+}
